381381c5e48b520552bed2833f58bdab30a1dd15
vadym <vadym@vpravi.onmicrosoft.com>
2022-01-25 17:20:29 +0200

    нові методи

--- vkursi-api-example/token/AuthorizeClass.cs
+++ vkursi-api-example/token/AuthorizeClass.cs
@@ -26,8 +26,8 @@ namespace vkursi_api_example.token
            
             AuthorizeRequestBodyModel ARBody = new AuthorizeRequestBodyModel
             {
-                Email = "admin@admin.com",       // Логін (Email)
-                Password = "admin1qaz2wsx3EDC"                 // Пароль
+                Email = "test@testemail.com",       // Логін (Email)
+                Password = "123456"                 // Пароль
             };
 
             AuthorizeResponseModel AuthorizeResponse = new AuthorizeResponseModel();
