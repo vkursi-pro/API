2fa6a7a58762279c45599b80cba9f9b749bfed65
Roma <roman@vpravi.onmicrosoft.com>
2024-05-29 17:54:49 +0300

    Fix

--- vkursi-api-example/token/AuthorizeClass.cs
+++ vkursi-api-example/token/AuthorizeClass.cs
@@ -27,8 +27,8 @@ namespace vkursi_api_example.token
 
             AuthorizeRequestBodyModel ARBody = new AuthorizeRequestBodyModel
             {
-                Email = "akaShalffei@gmail.com",       // Логін (Email)
-                Password = "TH_8sAFpXSSvZBf"          // Пароль
+                Email = "Логін (Email)",       // Логін (Email)
+                Password = "Пароль"          // Пароль
             };
 
             AuthorizeResponseModel AuthorizeResponse = new AuthorizeResponseModel();
